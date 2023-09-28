de06d6fc9b4a1be2c270c0bdee9cb985ecc617af
RtiBobV <14822173+rtigithub@users.noreply.github.com>
2023-09-27 22:52:39 -0400

    CodeMaid

--- CSharp/Applications/WpfLoggingDI/App.xaml.cs
+++ CSharp/Applications/WpfLoggingDI/App.xaml.cs
@@ -1,151 +1,150 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Drawing;
+﻿using System;
 using System.Reflection;
 using System.Threading;
 using System.Windows;
 using LogViewer.Wpf;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MsLogger.Core;
 using RandomLogging.Service;
 using WpfLoggingDI.ViewModels;
-using MsLogger.Core;
 
 namespace WpfLoggingDI;
 
 public partial class App
 {
-    #region Constructor
+     #region Constructor
 
-    public App()
-    {
-        // catch all unhandled errors
-        AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+     public App()
+     {
+          // catch all unhandled errors
+          AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
 
-        HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+          HostApplicationBuilder builder = Host.CreateApplicationBuilder();
 
-        builder
-            /*
-             * Note: For information on launch profiles for debugging,
-             *     see article: https://www.codeproject.com/Articles/5354478/NET-App-Settings-Demystified-Csharp-VB
-             */
+          builder
+              /*
+               * Note: For information on launch profiles for debugging,
+               *     see article: https://www.codeproject.com/Articles/5354478/NET-App-Settings-Demystified-Csharp-VB
+               */
 
-            // Register the Random Logging Service
-            .AddRandomBackgroundService()
+              // Register the Random Logging Service
+              .AddRandomBackgroundService()
 
-            // visual debugging tools
-            .AddLogViewer()
+              // visual debugging tools
+              .AddLogViewer()
 
-            // Microsoft Logger
-            .Logging.AddDefaultDataStoreLogger();
+              // Microsoft Logger
+              .Logging.AddDefaultDataStoreLogger();
 
-            // uncomment to use custom logging colors (note: System.Drawing namespace)
-            //
-            //.Logging.AddDefaultDataStoreLogger(options =>
-            //{
-            //    options.Colors[LogLevel.Trace] = new()
-            //    {
-            //        Foreground = Color.White,
-            //        Background = Color.DarkGray
-            //    };
+          // uncomment to use custom logging colors (note: System.Drawing namespace)
+          //
+          //.Logging.AddDefaultDataStoreLogger(options =>
+          //{
+          //    options.Colors[LogLevel.Trace] = new()
+          //    {
+          //        Foreground = Color.White,
+          //        Background = Color.DarkGray
+          //    };
 
-            //    options.Colors[LogLevel.Debug] = new()
-            //    {
-            //        Foreground = Color.White,
-            //        Background = Color.Gray
-            //    };
+          //    options.Colors[LogLevel.Debug] = new()
+          //    {
+          //        Foreground = Color.White,
+          //        Background = Color.Gray
+          //    };
 
-            //    options.Colors[LogLevel.Information] = new()
-            //    {
-            //        Foreground = Color.White,
-            //        Background = Color.DodgerBlue
-            //    };
+          //    options.Colors[LogLevel.Information] = new()
+          //    {
+          //        Foreground = Color.White,
+          //        Background = Color.DodgerBlue
+          //    };
 
-            //    options.Colors[LogLevel.Warning] = new()
-            //    {
-            //        Foreground = Color.White,
-            //        Background = Color.Orchid
-            //    };
-            //});
+          //    options.Colors[LogLevel.Warning] = new()
+          //    {
+          //        Foreground = Color.White,
+          //        Background = Color.Orchid
+          //    };
+          //});
 
-        IServiceCollection services = builder.Services;
+          IServiceCollection services = builder.Services;
 
-        services
-            .AddSingleton<MainViewModel>()
-            .AddSingleton<MainWindow>(service => new MainWindow
-            {
-                DataContext = service.GetRequiredService<MainViewModel>()
-            });
+          services
+              .AddSingleton<MainViewModel>()
+              .AddSingleton<MainWindow>(service => new MainWindow
+              {
+                   DataContext = service.GetRequiredService<MainViewModel>()
+              });
 
-        _host = builder.Build();
-        _cancellationTokenSource = new();
-    }
+          _host = builder.Build();
+          _cancellationTokenSource = new();
+     }
 
-    #endregion
+     #endregion Constructor
 
-    #region Fields
+     #region Fields
 
-    private readonly IHost? _host;
-    private readonly CancellationTokenSource _cancellationTokenSource;
+     private readonly IHost? _host;
+     private readonly CancellationTokenSource _cancellationTokenSource;
 
-    #endregion
+     #endregion Fields
 
-    #region Methods
+     #region Methods
 
-    private void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
-        => MessageBox.Show(
-            ((Exception)e.ExceptionObject).Message,
-            "Unhandled Error",
-            MessageBoxButton.OK,
-            MessageBoxImage.Stop);
+     private void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+         => MessageBox.Show(
+             ((Exception)e.ExceptionObject).Message,
+             "Unhandled Error",
+             MessageBoxButton.OK,
+             MessageBoxImage.Stop);
 
-    protected override void OnStartup(StartupEventArgs e)
-    {
-        try
-        {
-            LogStartingMode();
+     protected override void OnStartup(StartupEventArgs e)
+     {
+          try
+          {
+               LogStartingMode();
 
-            // set and show
-            MainWindow = _host!.Services.GetRequiredService<MainWindow>();
-            MainWindow.Show();
+               // set and show
+               MainWindow = _host!.Services.GetRequiredService<MainWindow>();
+               MainWindow.Show();
 
-            // startup background services
-            _ = _host.StartAsync(_cancellationTokenSource.Token);
-        }
-        catch (OperationCanceledException)
-        {
-            // skip
-        }
-        catch (Exception ex)
-        {
-            MessageBox.Show(ex.Message, "Unhandled Error", MessageBoxButton.OK, MessageBoxImage.Stop);
-        }
-    }
+               // startup background services
+               _ = _host.StartAsync(_cancellationTokenSource.Token);
+          }
+          catch (OperationCanceledException)
+          {
+               // skip
+          }
+          catch (Exception ex)
+          {
+               MessageBox.Show(ex.Message, "Unhandled Error", MessageBoxButton.OK, MessageBoxImage.Stop);
+          }
+     }
 
-    protected override void OnExit(ExitEventArgs e)
-    {
-        // tell the background services that we are shutting down
-        _host!.StopAsync(_cancellationTokenSource.Token);
+     protected override void OnExit(ExitEventArgs e)
+     {
+          // tell the background services that we are shutting down
+          _host!.StopAsync(_cancellationTokenSource.Token);
 
-        base.OnExit(e);
-    }
+          base.OnExit(e);
+     }
 
-    private void LogStartingMode()
-    {
-        // Get the Launch mode
-        bool isDevelopment = string.Equals(Environment.GetEnvironmentVariable("DOTNET_MODIFIABLE_ASSEMBLIES"), "debug",
-                                           StringComparison.InvariantCultureIgnoreCase);
+     private void LogStartingMode()
+     {
+          // Get the Launch mode
+          bool isDevelopment = string.Equals(Environment.GetEnvironmentVariable("DOTNET_MODIFIABLE_ASSEMBLIES"), "debug",
+                                             StringComparison.InvariantCultureIgnoreCase);
 
-        // initialize a logger & EventId
-        ILogger<App> logger = _host!.Services.GetRequiredService<ILogger<App>>();
-        EventId eventId = new EventId(id: 0, name: Assembly.GetEntryAssembly()!.GetName().Name);
-        
-        // log a test pattern for each log level
-        logger.TestPattern(eventId: eventId);
+          // initialize a logger & EventId
+          ILogger<App> logger = _host!.Services.GetRequiredService<ILogger<App>>();
+          EventId eventId = new EventId(id: 0, name: Assembly.GetEntryAssembly()!.GetName().Name);
 
-        // log that we have started...
-        logger.Emit(eventId, LogLevel.Information, $"Running in {(isDevelopment ? "Debug" : "Release")} mode");
-    }
+          // log a test pattern for each log level
+          logger.TestPattern(eventId: eventId);
 
-    #endregion
+          // log that we have started...
+          logger.Emit(eventId, LogLevel.Information, $"Running in {(isDevelopment ? "Debug" : "Release")} mode");
+     }
+
+     #endregion Methods
 }
--- CSharp/Applications/WpfLoggingDI/ViewModels/MainViewModel.cs
+++ CSharp/Applications/WpfLoggingDI/ViewModels/MainViewModel.cs
@@ -5,18 +5,18 @@ namespace WpfLoggingDI.ViewModels;
 
 public class MainViewModel : ViewModel
 {
-    #region Constructor
+     #region Constructor
 
-    public MainViewModel(LogViewerControlViewModel logViewer)
-    {
-        LogViewer = logViewer;
-    }
+     public MainViewModel(LogViewerControlViewModel logViewer)
+     {
+          LogViewer = logViewer;
+     }
 
-    #endregion
+     #endregion Constructor
 
-    #region Properties
+     #region Properties
 
-    public LogViewerControlViewModel LogViewer { get; }
+     public LogViewerControlViewModel LogViewer { get; }
 
-    #endregion
+     #endregion Properties
 }
